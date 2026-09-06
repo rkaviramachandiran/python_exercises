109df1444031eb81d98aa6599fce57afc384732a
portfoilo <rkavi2964@gmail.com>
2026-09-06 10:59:45 +0530

    modified-5

---
+++ day_2-conditionals/14_odd_or_even.py
@@ -0,0 +1,19 @@
+#get input from user
+
+number=int(input("Enter your number :"))
+
+#check the number by using if conditon
+
+if number%2==0:
+    print(f"number {number} is a even number")
+else:
+    print(f"number {number} is a odd number")
+
+
+# sample output 1
+# Enter your number :20
+# number 20 is a even number
+
+# sample output 2
+# Enter your number :15
+# number 15 is a odd number
---
+++ day_2-conditionals/15_positive_or_negative.py
@@ -0,0 +1,26 @@
+# get input from user
+
+number=int(input("Enter your number :"))
+
+# check if the number positive or negative 
+
+if number > 0:
+    print("Given number is positive")
+
+elif number < 0:
+    print("Given number is negative")
+
+else :
+    print("Given number is zero")
+
+# sample output 1
+# Enter your number :10
+# Given number is positive
+
+# sample output 2
+# Enter your number :-20
+# Given number is negative
+
+# sample output 3
+# Enter your number :0
+# Given number is zero
